e273eb94faa8d1e3febb5a79d7b114b3591f0f04
Anton Mikhailov <tony.mikhailov@gmail.com>
2025-10-04 15:46:03 +0300

    * try set sys clock 112000

--- ztester.c
+++ ztester.c
@@ -1,22 +1,28 @@
 #include "pico/stdlib.h"
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
+#include "hardware/sync.h"
 #include "pio_squarewave.pio.h" // Автоматически сгенерированный файл
 
 #define OUTPUT_PIN 14
+
+#define PICO_CLOCK_ADJUST_PERI_CLOCK_WITH_SYS_CLOCK 1
 
 int main() {
     PIO pio = pio0;
     uint sm = pio_claim_unused_sm(pio, true);
     uint offset = pio_add_program(pio, &squarewave_program);
 
+    set_sys_clock_khz(112000, true);
     // Пример для частоты 56 МГц (N=16)
 //    float desired_freq = 3.5e6 * 16; // 56 МГц
 //    float desired_freq = 3.5e6;
-    float desired_freq = 28e6;
+//    float desired_freq = 14e6;
+    float desired_freq = 56e6;
     float div = (float)clock_get_hz(clk_sys) / (2 * desired_freq); // 125,000,000 / (2 * 56,000,000) ≈ 1.116
 
     squarewave_program_init(pio, sm, offset, OUTPUT_PIN, div);
+    save_and_disable_interrupts();
 
     while (true) {
         tight_loop_contents();
